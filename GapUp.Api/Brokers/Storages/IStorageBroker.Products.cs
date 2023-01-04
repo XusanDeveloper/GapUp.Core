10ffbb0bb5e65cdbe7517f04a546257be21e462f
huseyntechs <xusannematovdes@gmail.com>
2023-01-04 13:56:05 +0500

    BROKERS: CRUD for `Products`

---
+++ GapUp.Api/Brokers/Storages/IStorageBroker.Products.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System;
+using GapUp.Api.Models;
+
+namespace GapUp.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Product> InsertProductAsync(Product product);
+        IQueryable<Product> SelectAllProducts();
+        ValueTask<Product> SelectProductByIdAsync(Guid id);
+        ValueTask<Product> UpdateProductAsync(Product product);
+        ValueTask<Product> DeleteProductAsync(Product product);
+    }
+}
